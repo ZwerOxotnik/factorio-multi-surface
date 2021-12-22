2a3bd8b7dd8ecd65148311bd4edb88f111c5d989
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 23:42:48 +0300

    Update to 0.3.5

--- control.lua
+++ control.lua
@@ -1,1 +1,5 @@
-require("event_handler").add_lib(require("multi-surface-control"))
+if script.active_mods["zk-lib"] then
+	require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua").add_lib(require("multi-surface-control"))
+else
+	require("event_handler").add_lib(require("multi-surface-control"))
+end
